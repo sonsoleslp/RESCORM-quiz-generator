96e1ff540585004c26e63747adc5ed6a57505464
Sonsoles López Pernas <slopezpe@VPK4G9262M.local>
2022-12-20 22:35:46 +0100

    Add docsg

---
+++ docs/precache-manifest.b2e56355c2b6c2d5da6308e41e96880f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f5e4113a9d8e414ffe42",
+    "url": "./static/js/main.f5e4113a.chunk.js"
+  },
+  {
+    "revision": "0af004d3c497a92c9982",
+    "url": "./static/js/1.0af004d3.chunk.js"
+  },
+  {
+    "revision": "f5e4113a9d8e414ffe42",
+    "url": "./static/css/main.e5c6f10c.chunk.css"
+  },
+  {
+    "revision": "0859a053c1f034c426aaca8c7d496a79",
+    "url": "./index.html"
+  }
+];
